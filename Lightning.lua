98c32dc37da4dd56469add76de4147cfea4040df
Mikael Lind <elemel@elemel.se>
2015-08-27 21:25:11 +0200

    Moar improved lightning

--- Lightning.lua
+++ Lightning.lua
@@ -14,14 +14,18 @@ function Lightning.new(args)
   lightning.y2 = args.y2 or 0
 
   lightning.maxLength = args.maxLength or 1
-  lightning.maxSlope = args.maxSlope or 1
-  lightning.maxWidth = args.maxWidth or 1
+  lightning.maxSlope = args.maxSlope or 0.75
 
-  lightning.minTtl = 3 / 32
-  lightning.maxTtl = 3 / 16
+  lightning.minTtl = 1 / 8
+  lightning.maxTtl = 1 / 4
 
   lightning.vertices = {lightning.x1, lightning.y1, lightning.x2, lightning.y2}
   lightning.ttls = {}
+
+  lightning.minResetDelay = 1
+  lightning.maxResetDelay = 2
+
+  lightning.resetDelay = common.mix(lightning.minResetDelay, lightning.maxResetDelay, love.math.random())
 
   game.updates.physics[lightning] = Lightning.update
   game.draws.particles[lightning] = Lightning.draw
@@ -34,6 +38,11 @@ function Lightning:destroy()
   game.updates.physics[self] = nil
 end
 
+function Lightning:reset()
+  self.vertices = {self.x1, self.y1, self.x2, self.y2}
+  self.ttls = {}
+end
+
 function Lightning:update(dt)
   local adam = game.names.adam
 
@@ -44,18 +53,43 @@ function Lightning:update(dt)
     self.y2 = adam.y - 0.75
   end
 
-  self.vertices[1] = self.x1
-  self.vertices[2] = self.y1
+  self.resetDelay = self.resetDelay - dt
 
-  self.vertices[#self.vertices - 1] = self.x2
-  self.vertices[#self.vertices] = self.y2
+  if self.resetDelay < 0 then
+    self:reset()
+    self.resetDelay = common.mix(self.minResetDelay, self.maxResetDelay, love.math.random())
+  else
+    self.vertices[1] = self.x1
+    self.vertices[2] = self.y1
+
+    self.vertices[#self.vertices - 1] = self.x2
+    self.vertices[#self.vertices] = self.y2
+  end
+
+  local length = math.sqrt((self.x2 - self.x1) ^ 2 + (self.y2 - self.y1) ^ 2)
+
+  local tangentX = (self.x2 - self.x1) / length
+  local tangentY = (self.y2 - self.y1) / length
+
+  local normalX, normalY = -tangentY, tangentX
 
   local i = 1
 
   while i <= #self.ttls do
+    local x = self.vertices[2 * i + 1]
+    local y = self.vertices[2 * i + 2]
+
+    local tangentOffset = common.dot(x - self.x1, y - self.y1, tangentX, tangentY)
+    local normalOffset = common.dot(x - self.x1, y - self.y1, normalX, normalY)
+
+    if math.abs(normalOffset) > 0.25 * length then
+      self:reset()
+      break
+    end
+
     self.ttls[i] = self.ttls[i] - dt
 
-    if self.ttls[i] < 0 then
+    if self.ttls[i] < 0 or tangentOffset < 0 or tangentOffset > length then
       table.remove(self.vertices, 2 * i + 1)
       table.remove(self.vertices, 2 * i + 1)
 
